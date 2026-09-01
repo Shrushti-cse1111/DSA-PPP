8e2151d125126c87d93978a96af450659d988cc6
Shrushti Angadi <shrushti.angadi_comp25@pccoer.in>
2026-09-01 12:04:07 +0530

    day3 code added

---
+++ day3.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class day3 { 
+
+    // // Method to calculate the tip based on bill amount and rating
+    // public static float calculateTip(float billAmount, int rating) {
+    //     float tipAmount = 0.0f;
+        
+    //     if (rating == 1) {
+    //         tipAmount = billAmount * 0.05f; // 5% tip
+    //     } else if (rating == 2) {
+    //         tipAmount = billAmount * 0.08f; // 8% tip
+    //     } else if (rating == 3) {
+    //         tipAmount = billAmount * 0.10f; // 10% tip
+    //     } else if (rating == 4) {
+    //         tipAmount = billAmount * 0.15f; // 15% tip
+    //     } else if (rating == 5) {
+    //         tipAmount = billAmount * 0.20f; // 20% tip
+    //     }
+    //     return tipAmount;
+    // }
+
+    //Method to search a Book 
+
+    public static void searchBook(String title, int year){
+
+    }
+    
+    public void calculateFine (int daysOverdue){
+
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in); 
+        // String name = "Surrinder"; 
+        // float billAmount;
+        // int rating;
+        // float tipAmount;
+
+        // System.out.print("Enter the bill amount: ");
+        // billAmount = sc.nextFloat();
+
+        // System.out.print("Enter rating (1-5): ");
+        // rating = sc.nextInt();
+
+        // // Pass arguments and store the returned tip amount
+        // tipAmount = calculateTip(billAmount, rating);
+
+        // System.out.println(name + ", your tip amount is \u20B9: " + tipAmount);
+        
+        int authors ;
+
+        System.out.println("Enter book name ");
+        
+        sc.close(); 
+    }
+}
+
